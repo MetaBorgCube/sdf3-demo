d9122514d23ded431585bafba840ac1c6cf2dbfe
Eelco Visser <e.visser@tudelft.nl>
2016-05-24 15:57:19 +0200

    project to demonstrate SDF3 features with syntax of SDF3 in three flavors: s-expressions, ambiguous expression syntax, disambiguated expression syntax

---
+++ sdf3-demo/src/main/strategies/sdf_demo/strategies/InteropRegisterer.java
@@ -0,0 +1,10 @@
+package sdf_demo.strategies;
+
+import org.strategoxt.lang.JavaInteropRegisterer;
+import org.strategoxt.lang.Strategy;
+
+public class InteropRegisterer extends JavaInteropRegisterer {
+    public InteropRegisterer() {
+        super(new Strategy[] { });
+    }
+}
---
+++ sdf3-demo/src/main/strategies/sdf_demo/strategies/Main.java
@@ -0,0 +1,9 @@
+package sdf_demo.strategies;
+
+import org.strategoxt.lang.Context;
+
+public class Main {    
+    public static void init(Context context) {
+        
+    }
+}
